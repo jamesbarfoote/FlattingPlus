8745d47ea37a7d4dab17ec4c6912f37d229e315b
James Barfoote <james.barfoote@gmail.com>
2016-02-26 19:50:40 +1300

    date and time pickers done and can go back by clicking done

--- app/src/main/java/com/example/dinoapps/flattingplus/EventPickerActivity.java
+++ app/src/main/java/com/example/dinoapps/flattingplus/EventPickerActivity.java
@@ -1,17 +1,23 @@
 package com.example.dinoapps.flattingplus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
 import com.codetroopers.betterpickers.radialtimepicker.RadialTimePickerDialogFragment;
-import com.codetroopers.betterpickers.timepicker.TimePickerBuilder;
 
 public class EventPickerActivity extends AppCompatActivity implements CalendarDatePickerDialogFragment.OnDateSetListener, RadialTimePickerDialogFragment.OnTimeSetListener{
     TextView startDate;
     TextView timeDate;
+    String time;
+    String date;
+    String description;
+    Button doneB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class EventPickerActivity extends AppCompatActivity implements CalendarDa
 
         this.startDate = (TextView) findViewById(R.id.startDateTxt);
         this.timeDate = (TextView) findViewById(R.id.timeTxt);
+        this.doneB = (Button) findViewById(R.id.doneButtonPicker);
 
         this.startDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,16 +54,54 @@ public class EventPickerActivity extends AppCompatActivity implements CalendarDa
             }
 
         });
+
+
+        this.doneB.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Log.v("Done","Done clicked");
+                TextView dec = (TextView)findViewById(R.id.eventDiscriptionTxt);
+                setDesc(dec.toString());
+                //Go back to calendar fragment
+                //Intent n = new Intent(v.getContext(), CalendarFragment.class);
+                //startActivity(new Intent(v.getContext(), CalendarFragment.class));
+                finish();
+            }
+        });
+    }
+
+    public void setDesc(String description)
+    {
+        this.description = description;
     }
 
     @Override
     public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
         startDate.setText(getString(R.string.calendar_date_picker_result_values, year, monthOfYear, dayOfMonth));
+        this.date = dayOfMonth + "," + monthOfYear + "," + year;
     }
 
     @Override
     public void onTimeSet(RadialTimePickerDialogFragment dialog, int hourOfDay, int minute) {
         timeDate.setText(getString(R.string.radial_time_picker_result_value, hourOfDay, minute));
+        this.time = hourOfDay + ":" + minute;
     }
+
+    public String getDate()
+    {
+        return this.date;
+    }
+
+    public String getDateTime()
+    {
+        return this.time;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
 
 }
