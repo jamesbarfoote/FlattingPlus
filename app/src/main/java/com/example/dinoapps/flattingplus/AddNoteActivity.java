1c3b63a1df05c2033fcfce26467cedf75c3a9d21
James Barfoote <james.barfoote@gmail.com>
2016-06-30 20:00:42 +1200

    added method to upload new note

--- app/src/main/java/com/example/dinoapps/flattingplus/AddNoteActivity.java
+++ app/src/main/java/com/example/dinoapps/flattingplus/AddNoteActivity.java
@@ -10,10 +10,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddNoteActivity extends AppCompatActivity {
     EditText titleText;
     EditText notesText;
     DBHelper dbHelper= new DBHelper(this);
+    String TAG = "Add Note Activity";
+    RequestQueue queue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,7 @@ public class AddNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_note);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        queue = Volley.newRequestQueue(this);
 
         titleText = (EditText) findViewById(R.id.newNoteTitleTxt);
         notesText = (EditText) findViewById(R.id.newNoteTxt);
@@ -64,5 +82,59 @@ public class AddNoteActivity extends AppCompatActivity {
         Log.v("Add note activity", "Email: " + userEmail + " Group: " + flatgroup + " Time created: " + ts);
 
         MainActivity.dbHelper.insertNote(userEmail,title, note, flatgroup, ts);
+        uploadNote(userEmail, flatgroup, ts, title, note);
+    }
+
+    public void uploadNote(String userEmail, String flatgroup, String ts, String title, String note)
+    {
+        Log.v(TAG, userEmail + " " + flatgroup + " " + ts + " " + title + " " + note);
+        String baseURL = "https://flattingplus.herokuapp.com";
+        String url = baseURL + "/add/note";
+
+         /*Post data*/
+        Map<String, String> jsonParams = new HashMap<String, String>();
+
+        JSONObject notes = new JSONObject();
+        try {
+            notes.put("group", flatgroup);
+            notes.put("notetitle", title);
+            notes.put("notecontent", note);
+            notes.put("notecreator", userEmail);
+            notes.put("notetimestamp", ts);
+
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest postRequest = new JsonObjectRequest( Request.Method.PUT, url, notes,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try
+                        {
+                            Log.v(TAG, "note added: " + response.toString(4));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //   Handle Error
+                        Log.v(TAG, "Error: " + error);
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                headers.put("User-agent", System.getProperty("http.agent"));
+                return headers;
+            }
+        };
+        queue.add(postRequest);
     }
 }
